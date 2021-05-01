0adb078c8f8f96dd2ed7bcd378f0ac514f10876f
sidharth panda <sidharthpandarpg1@gmail.com>
2021-05-01 09:31:10 +0530

    indianpanda

---
+++ whh.js
@@ -0,0 +1,9 @@
+Hackers who do legal works or work for goverment organisations are called white hat hackers. 
+These hackers are basically certified ethical hackers.They are authorized or certified to hack Systems.
+This hacking is done to test the level of cybersecurity in their organisation. by doing so they identify
+the weak points and fix them to avoid attacks from external sources.These hackers work as per rules and regulation
+set by the government. 
+
+    Now if you want to know about others go back to the main page
+
+
