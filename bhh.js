172419fb6d94bc408b799e99e5e0006499ddceab
Sidharth panda <80633567+coderpanda11@users.noreply.github.com>
2021-05-19 10:24:52 +0530

    Add files via upload

---
+++ bhh.js
@@ -0,0 +1,7 @@
+Black Hat hackers are the ones with poor intentions. These people, like all hackers, have somewhat advanced knowledge of computers and use these skills to spread malware, steal credentials or wreak havoc on somebody’s else’s system. 
+The key here is that these hackers were not given permission to break into a system.
+Most cyber criminals have hopes of financial gain by holding someone else’s data “hostage” and requiring compensation as a ransom or using credentials to gain access to a bank account that is not theirs.
+Another reason might be for the pure thrill of exercising their skills and knowledge in the performance of illegal acts.
+
+
+                            Now if you want to know about others go back to the main page
